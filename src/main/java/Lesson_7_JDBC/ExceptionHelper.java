145afe6c6f4a934e187548254c6305fe5ca344e4
oivaniv <oivaniv@n-ix.com>
2021-03-17 01:24:47 +0200

    Homework for Lesson 7 (JDBC), wrote classes for Create, Insert, Select data to the DB

---
+++ src/main/java/Lesson_7_JDBC/ExceptionHelper.java
@@ -0,0 +1,38 @@
+package Lesson_7_JDBC;
+
+import java.sql.BatchUpdateException;
+import java.sql.SQLException;
+
+public class ExceptionHelper {
+    public static void printSQLException(SQLException exception) {
+        for (Throwable e : exception) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = exception.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+    public static void printBatchUpdateException(BatchUpdateException batchUpdateException) {
+        System.err.println("--------BatchUpdateException--------");
+        System.err.println("SQLState: " +batchUpdateException.getSQLState());
+        System.err.println("Message: " +batchUpdateException.getMessage());
+        System.err.println("Vendor: " +batchUpdateException.getErrorCode());
+        System.err.print("Update counts: ");
+        int[] updateCounts = batchUpdateException.getUpdateCounts();
+
+        for (int i =0;i<updateCounts.length;i++){
+            System.err.print(updateCounts[i] + " ");
+        }
+
+    }
+
+
+}
---
+++ src/main/java/Lesson_7_JDBC/WriteDataToJDBC.java
@@ -0,0 +1,93 @@
+package Lesson_7_JDBC;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class WriteDataToJDBC {
+
+    public void insertDataToJDBC(String insertQuery) {
+        try {
+            PreparedStatement preparedStatement = SetupConnectionToJDBC.setupConnectionToJDBC().prepareStatement(insertQuery);
+
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "Samsung");
+            preparedStatement.setString(3, "TV");
+            preparedStatement.setInt(4, 1000);
+
+            System.out.println(preparedStatement);
+
+            int result = preparedStatement.executeUpdate();
+
+            System.out.println(result + " rows ");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    //This one doesn't work with using conenction from another class
+    public void batchInsertToJDBC() {
+        try {
+            Statement statement = SetupConnectionToJDBC.setupConnectionToJDBC().createStatement();
+            SetupConnectionToJDBC.setupConnectionToJDBC().setAutoCommit(false);
+
+            statement.addBatch("INSERT INTO Devices VALUES (1, 'Samsung', 'TV', 1000);");
+            statement.addBatch("INSERT INTO Devices VALUES (2, 'LG', 'Phone', 265);");
+            statement.addBatch("INSERT INTO Devices VALUES (3, 'Canon', 'Camera', 403);");
+            statement.addBatch("INSERT INTO Devices VALUES (4, 'Samsung', 'Phone', 334);");
+            statement.addBatch("INSERT INTO Devices VALUES (5, 'Sony', 'TV', 890);");
+
+            int[] createdCount = statement.executeBatch();
+
+            System.out.println(Arrays.toString(createdCount));
+
+            SetupConnectionToJDBC.setupConnectionToJDBC().commit();
+
+        } catch (
+                BatchUpdateException batchUpdateException) {
+            ExceptionHelper.printBatchUpdateException(batchUpdateException);
+        } catch (
+                SQLException e) {
+            ExceptionHelper.printSQLException(e);
+        }
+    }
+
+    public void batchInsertToJDBCWithNewConnection() {
+
+        String url = "jdbc:mysql://localhost:3306/company?useSSL=false";
+        String userName = "root";
+        String password = "123qwe123";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+
+            Statement statement = connection.createStatement();
+
+            connection.setAutoCommit(false);
+
+            statement.addBatch("INSERT INTO Devices VALUES (1, 'Samsung', 'TV', 1000);");
+            statement.addBatch("INSERT INTO Devices VALUES (2, 'LG', 'Phone', 265);");
+            statement.addBatch("INSERT INTO Devices VALUES (3, 'Canon', 'Camera', 403);");
+            statement.addBatch("INSERT INTO Devices VALUES (4, 'Samsung', 'Phone', 334);");
+            statement.addBatch("INSERT INTO Devices VALUES (5, 'Sony', 'TV', 890);");
+
+            int[] createdCount = statement.executeBatch();
+
+            System.out.println(Arrays.toString(createdCount));
+
+            connection.commit();
+
+        } catch (
+                BatchUpdateException batchUpdateException) {
+            ExceptionHelper.printBatchUpdateException(batchUpdateException);
+        } catch (
+                SQLException e) {
+            ExceptionHelper.printSQLException(e);
+        }
+    }
+
+
+}
